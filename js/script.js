12336ce04e30359780204b141d6f23b8c471c5c5
Rakhi Datta <146545970+rakhidatta26@users.noreply.github.com>
2023-12-26 20:27:10 +0600

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,11 @@
+var swiper = new Swiper(".swiper", {
+  slidesPerView: 3,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+});
